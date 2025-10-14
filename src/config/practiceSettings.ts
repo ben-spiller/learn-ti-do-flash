54da69c9d2f791dd642d2e1e6cc5e0e65214a2ee
Ben Spiller <ben-spiller@users.noreply.github.com>
2025-10-14 20:21:19 +0100

    move unwanted constants back to Settings page

--- src/config/practiceSettings.ts
+++ src/config/practiceSettings.ts
@@ -21,29 +21,6 @@ export class PracticeSettings {
   }
 }
 
-// Solfege mappings
-export const SOLFEGE_TO_INTERVAL: Record<string, number> = {
-  "Do": 0,
-  "Re": 2,
-  "Mi": 4,
-  "Fa": 5,
-  "Sol": 7,
-  "La": 9,
-  "Ti": 11,
-};
-
-export const INTERVAL_TO_SOLFEGE: Record<number, string> = {
-  0: "Do",
-  2: "Re",
-  4: "Mi",
-  5: "Fa",
-  7: "Sol",
-  9: "La",
-  11: "Ti",
-};
-
-export const SOLFEGE_NOTES = ["Do", "Re", "Mi", "Fa", "Sol", "La", "Ti"];
-
 // Instrument options
 export const INSTRUMENT_OPTIONS = [
   { slug: "acoustic_grand_piano", label: "Grand Piano" },
@@ -52,16 +29,9 @@ export const INSTRUMENT_OPTIONS = [
   { slug: "saxophone", label: "Saxophone" },
 ];
 
-// Root note pitch options
-export const ROOT_NOTE_OPTIONS = [
-  "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
-  "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
-  "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
-];
-
-// Constraints
+// Constraints on valid values
 export const CONSTRAINTS = {
   numberOfNotes: { min: 2, max: 10 },
-  tempo: { min: 40, max: 200, step: 5 },
+  tempo: { min: 40, max: 300, step: 5 },
   interval: { min: 1, max: 7 },
 } as const;
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -14,11 +14,7 @@ import {
 } from "@/utils/audio";
 import {
   PracticeSettings,
-  SOLFEGE_TO_INTERVAL,
-  INTERVAL_TO_SOLFEGE,
-  SOLFEGE_NOTES,
   INSTRUMENT_OPTIONS,
-  ROOT_NOTE_OPTIONS,
   CONSTRAINTS,
 } from "@/config/practiceSettings";
 
@@ -95,6 +91,36 @@ const Settings = () => {
     })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  // TODO: this is a crazy way to do it - refactor this to something more compact
+  const SOLFEGE_TO_INTERVAL: Record<string, number> = {
+    "Do": 0,
+    "Re": 2,
+    "Mi": 4,
+    "Fa": 5,
+    "Sol": 7,
+    "La": 9,
+    "Ti": 11,
+  };
+
+  const INTERVAL_TO_SOLFEGE: Record<number, string> = {
+    0: "Do",
+    2: "Re",
+    4: "Mi",
+    5: "Fa",
+    7: "Sol",
+    9: "La",
+    11: "Ti",
+  };
+
+  // Root note pitch options
+  const ROOT_NOTE_OPTIONS = [
+    "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+    "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+    "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+  ];
+
+  const SOLFEGE_NOTES = ["Do", "Re", "Mi", "Fa", "Sol", "La", "Ti"];
 
   return (
     <div className="min-h-screen bg-background flex items-center justify-center p-4">
