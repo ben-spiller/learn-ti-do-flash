5c921580db823420e606001c5c1587a7c20ceb54
Ben Spiller <ben-spiller@users.noreply.github.com>
2025-10-15 19:27:37 +0100

    use hashrouter to avoid GH pages problems

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import SettingsView from "./pages/Settings";
 import PracticeView from "./pages/Practice";
 import NotFound from "./pages/NotFound";
@@ -37,14 +37,14 @@ const App = () => {
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={basename}>
+      <HashRouter basename={basename}>
         <Routes>
           <Route path="/" element={<SettingsView />} />
           <Route path="/practice" element={<PracticeView />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
   );
