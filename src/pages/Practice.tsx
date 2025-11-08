12a910c7fe6876596ff004ca7f74333bf7dfb51b
Ben Spiller <ben-spiller@users.noreply.github.com>
2025-11-08 22:30:12 +0000

    upper bound on needs practice

--- src/pages/Practice.tsx
+++ src/pages/Practice.tsx
@@ -321,8 +321,10 @@ const PracticeView = () => {
 
       // Add to needsPractice for the CORRECT note (+3 if first wrong answer or in the danger zone, +1 otherwise)
       let needsPracticeCount = (needsPractice.current.get(pairKey) || 0);
-      needsPractice.current.set(pairKey, needsPracticeCount + (
-        (needsPracticeCount <3) ? +3 : +1));
+      // put an upper bound on the severity of each one to avoid it getting crazy
+      const maxNeedsPractice = 20;
+      needsPractice.current.set(pairKey, Math.min(maxNeedsPractice, needsPracticeCount + (
+        (needsPracticeCount <3) ? +3 : +1)));
       
       // Also increment needsPractice for the INCORRECT note that was entered
       const incorrectPairKey = `${prevInterval},${selectedNote}`;
