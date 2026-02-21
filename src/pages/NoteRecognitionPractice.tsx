d9a08fc825af8395ec1974c4a859af74d75212a1
Ben Spiller <ben-spiller@users.noreply.github.com>
2026-02-21 15:50:10 +0000

    remove unused

--- src/pages/NoteRecognitionPractice.tsx
+++ src/pages/NoteRecognitionPractice.tsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-import { stopSounds, MidiNoteNumber, SemitoneOffset, playNote, playSequence, semitonesToSolfege, midiToNoteName, noteNameToMidi, preloadInstrumentWithGesture, startDrone, stopDrone, setDroneVolume, semitonesToOneOctave, keypressToSemitones, isAudioInitialized, mustWaitForGestureBeforeAudioInit, startAudio, handleSemitoneModifierUp, handleSemitoneModifierDown } from "@/utils/audio";
+import { stopSounds, MidiNoteNumber, SemitoneOffset, playNote, playSequence, semitonesToSolfege, midiToNoteName, noteNameToMidi, startDrone, stopDrone, setDroneVolume, semitonesToOneOctave, keypressToSemitones, startAudio, handleSemitoneModifierUp, handleSemitoneModifierDown } from "@/utils/audio";
 import { ConfigData, ExerciseType } from "@/config/ConfigData";
 import { saveCurrentConfiguration } from "@/utils/settingsStorage";
 import { getGlobalSettings } from "@/utils/globalSettingsStorage";
